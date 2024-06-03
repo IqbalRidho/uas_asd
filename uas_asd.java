fec740d4e8b3e6416220b94af1cb9824e5d67521
IqbalRidho <iqbalridho768696@gmail.com>
2024-06-03 16:54:21 +0700

    Firtst commit

---
+++ uas_asd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class uas_asd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt(); // untuk jumlah gerbong
+        int M = scanner.nextInt(); // untuk jumlah aktivitas
+
+        LinkedList<Integer> gerbong = new LinkedList<>();
+    }
+}
